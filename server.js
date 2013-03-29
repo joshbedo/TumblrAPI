6cbcb4645e0a825144692c1f78fa4ff0ba498686
JoshBedo <joshbedo@yahoo.com>
2013-03-29 13:52:12 -0400

    creating sessions and setting token/token_secret

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ var express = require('express'),
 	tumblr  = require('tumblr.js'),
 	client  = {
 		consumer_key: '1n9fMPxCBFBbcIGRImYKSK5wwDL6yux64S4DxEwiwzHTNuaIoD',
-		consumer_secret: 'bW9YKbnwgxexyVx1AaxQr1QoemEkjd29p5U1WpbZ8r1XEH41C0'
+		consumer_secret: 'bW9YKbnwgxexyVx1AaxQr1QoemEkjd29p5U1WpbZ8r1XEH41C0',
 	},
 	consumer = new oauth.OAuth(
 		"http://www.tumblr.com/oauth/request_token", "http://www.tumblr.com/oauth/access_token",
-		client.consumer_key, client.consumer_secret, "1.0A", "http://127.0.0.1:8080/sessions/callback", "HMAC-SHA1");
+		client.consumer_key, client.consumer_secret, "1.0A", "http://localhost:1337/oauth/testapp", "HMAC-SHA1");
 
-server.listen(1337);
 
-app.configure('dev', function(){
+app.configure('development', function(){
 	app.use(express.logger());
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
@@ -27,8 +26,11 @@ app.get('/home', function(req, res){
 		if(err){
 			console.log(err);
 		}else{
-			//req.session.oauth.token = oauth_token;
+			console.log(req.session);
+			//client.oauth_token = oauth_token;
+			req.session.oauth = {token: oauth_token, token_secret: oauth_token_secret};
 			console.log('oauth.token: ' + oauth_token);
+			//client.oauth_token_secret = oauth_token_secret;
 			//req.session.oauth.token_secret = oauth_token_secret;
 			console.log('oauth.token_secret: ' + oauth_token_secret);
 			res.redirect('http://www.tumblr.com/oauth/authorize?oauth_token='+oauth_token);
@@ -37,5 +39,20 @@ app.get('/home', function(req, res){
 });
 
 app.get('/oauth/testapp', function(req, res){
-	res.send('callback');
-})
+	console.log(req.session.oauth);
+	var user = tumblr.createClient({
+			consumer_key: client.consumer_key,
+			consumer_secret: client.consumer_secret,
+			token: req.session.oauth.token,
+			token_secret: req.session.oauth.token_secret
+	});
+	user.userInfo(function(err, data){
+		if(!err){
+			console.log(data);
+		}else{
+			console.log(err);
+		}
+	});
+});
+
+server.listen(1337);
